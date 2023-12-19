23854bb31dd1d1ec1aff0328c097449def4bb235
oussmab4 <oussama.mabsout@edu.esiee.fr>
2023-12-19 15:24:33 +0100

    first commit

--- Calculs_Geo/src/main/java/BE/AC/UMONS/Perimetre.java
+++
@@ -1,7 +0,0 @@
-package BE.AC.UMONS;
-
-public class Perimetre {
-    public static int perim(int a, int b) {
-        return Produit.mult(Addition.add(a, b), 2);
-    }
-}
--- Calculs_Geo/src/main/java/BE/AC/UMONS/Surface.java
+++
@@ -1,7 +0,0 @@
-package BE.AC.UMONS;
-
-public class Surface {
-    public static int surf(int a, int b) {
-        return Produit.mult(a, b);
-    }
-}
